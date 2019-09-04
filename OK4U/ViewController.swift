2e540aa667f59a407a4d10639e5aae5929cbd630
Daniel Kim <kdanielive@gmail.com>
2019-09-04 09:35:09 -0400

    Initial commit

---
+++ OK4U/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 9/4/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
