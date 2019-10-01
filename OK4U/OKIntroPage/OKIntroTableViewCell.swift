c97cac850c288c68ebefeb60fd0c846148a59f51
Daniel Kim <kdanielive@gmail.com>
2019-10-01 19:50:30 -0400

    Adding member personal info to members page

---
+++ OK4U/OKIntroPage/OKIntroTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  OKIntroTableViewCell.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 10/1/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class OKIntroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
