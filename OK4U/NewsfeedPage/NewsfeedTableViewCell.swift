efc8cdea9096eddcfa416f7b6e4d48c3704484a1
Daniel Kim <kdanielive@gmail.com>
2019-11-13 18:15:52 -0500

    Created the Newsfeed tableview

---
+++ OK4U/NewsfeedPage/NewsfeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsfeedTableViewCell.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 11/13/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
