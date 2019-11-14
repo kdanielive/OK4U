c7c593f4760cee9208d86e4b842de5fa6cebb10a
Daniel Kim <kdanielive@gmail.com>
2019-11-14 01:12:36 -0500

    Made the eventdetailviewcontroller

---
+++ OK4U/NewsfeedPage/EventDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventDetailViewController.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 11/14/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
