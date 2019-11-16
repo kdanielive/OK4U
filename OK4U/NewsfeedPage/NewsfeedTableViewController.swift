756b6a0747b1f5e386bd8bdb434635326f58c22b
Daniel Kim <kdanielive@gmail.com>
2019-11-16 18:49:20 -0500

    Gonna get rid of tab bars

--- OK4U/NewsfeedPage/NewsfeedTableViewController.swift
+++ OK4U/NewsfeedPage/NewsfeedTableViewController.swift
@@ -13,12 +13,21 @@ class NewsfeedTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        self.navigationController?.navigationBar.isHidden = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnSwipe = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      navigationController?.hidesBarsOnSwipe = false
     }
 
     // MARK: - Table view data source
