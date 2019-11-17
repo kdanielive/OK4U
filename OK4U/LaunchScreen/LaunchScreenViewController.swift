8437b5a3d5582fbcbe392ebac517ea8a1df6ca82
Daniel Kim <kdanielive@gmail.com>
2019-11-16 23:28:49 -0500

    Set the launch screen

---
+++ OK4U/LaunchScreen/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 11/16/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 12/255, green: 67/255, blue: 46/255, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OK4U/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SideMenuViewController.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 11/16/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loginButton.layer.borderWidth = 1
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
