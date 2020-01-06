b9eb3d406fcf7c11b5aa72a2fbb42fc784272105
Daniel Kim <kdanielive@gmail.com>
2020-01-06 17:34:04 +0900

    Nightwalk viewcontroller added

---
+++ OK4U/NightwalkPage/NightwalkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NightwalkViewController.swift
+//  OK4U
+//
+//  Created by Daniel Kim on 1/6/20.
+//  Copyright © 2020 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class NightwalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
