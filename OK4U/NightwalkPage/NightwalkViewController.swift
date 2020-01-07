d6d4e8bb013cd5a1a0b01b9c3bfef7f69e199360
Daniel Kim <kdanielive@gmail.com>
2020-01-07 15:13:30 +0900

    Added navigation

--- OK4U/NightwalkPage/NightwalkViewController.swift
+++ OK4U/NightwalkPage/NightwalkViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 import CallKit
+import Mapbox
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
 
-class NightwalkViewController: UIViewController {
+
+class NightwalkViewController: UIViewController, MGLMapViewDelegate {
+    
+    @IBOutlet var navigationFrame: MGLMapView!
+    var mapView: NavigationMapView!
+    var directionsRoute: Route?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView = NavigationMapView(frame: navigationFrame.bounds)
+         
+        navigationFrame.addSubview(mapView)
+         
+        // Set the map view's delegate
+        mapView.delegate = self
+        
+        // Allow the map to display the user's location
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true, completionHandler: nil)
+
         // Do any additional setup after loading the view.
     }
 
