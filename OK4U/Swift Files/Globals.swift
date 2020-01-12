00a6ca389516b544729361cc862280cd95bf16d9
Daniel Kim <kdanielive@gmail.com>
2020-01-12 15:19:30 +0900

    Finished putting in all the preliminary buildings'

--- OK4U/Swift Files/Globals.swift
+++ OK4U/Swift Files/Globals.swift
@@ -54,84 +54,44 @@ let buildingsDict = [
     "East Campus": (40.806925, -73.959147),
     "Faculty House": (40.806559, -73.959318),
     "Fairchild": (40.808991, -73.960625),
-    "Fayerweather Hall": (40.808135,-73.960547),
+    "Fayerweather Hall": (40.808135, -73.960547),
     "Furnald": (40.807350, -73.963796),
     "Hartley": (40.806433, -73.961744),
     "Hamilton": (40.806774, -73.961738),
     "Heyman Center": (40.807479, -73.958888),
     "Hogan": (40.806321, -73.964684),
-    "International Affairs": (40.807733, -73.960128)
+    "International Affairs": (40.807733, -73.960128),
     "Jerome Greene": (40.806760, -73.960501),
     "John Jay": (40.806005, -73.962283),
     "Journalism": (40.807550, -73.963545),
     "Kappa Alpha Theta (534 West 114th Street)": (40.806065, -73.963639),
     "Kappa Delta Rho (548 West 114th Street)": (40.806252, -73.964057),
     "Kent": (40.807359, -73.961686),
-    
+    "Knox Hall": (40.811970, -73.961779),
+    "Lerner Hall": (40.806896, -73.963857),
+    "Lewisohn": (40.808375, -73.963111),
+    "Low Library": (40.808025, -73.961952),
+    "Mathematics": (40.808994, -73.962644),
+    "McBain": (40.805797, -73.964955),
+    "McVickar Hall": (40.806499, -73.966631),
+    "Mudd": (40.809311, -73.959899),
+    "Northwest Corner Building": (40.809945, -73.961920),
+    "Philosophy": (40.807430, -73.961035),
+    "Psi Upsilon (542 West 114th Street)": (40.806194, -73.963894),
+    "Pupin": (40.809928, -73.961350),
+    "River": (40.807202, -73.966339),
+    "Ruggles": (40.805681, -73.962804),
+    "Schapiro": (40.807830, -73.965298),
+    "Schermerhorn": (40.808683, -73.960427),
+    "Sigma Chi (523 West 113th Street)": (40.805429, -73.963565),
+    "Sigma Delta Tau (540 West 114th Street)": (40.805701, -73.964096),
+    "Sigma Nu (556 West 113th Street)": (40.805644, -73.964573),
+    "Sigma Phi Epsilon (550 West 113th Street)": (40.805587, -73.964413),
+    "Uris Hall": (40.808842, -73.961392),
+    "Wallach": (40.806146, -73.961962),
+    "Watt": (40.805853, -73.964511),
+    "Wien": (40.806660, -73.959796),
+    "Woodbridge": (40.808258, -73.966153),
+    "Zeta Psi (531 West 113th Street)": (40.805546, -73.963809)
 ]
 
-let temp = """
-Greene Annex
-Greenhouse Nursery
-Hartley
-Havemeyer Extension
-Havemeyer Hall
-Heyman Center
-Hogan
-Horace Mann
-Interchurch Center
-Intercultural Resource Center (552 West 114th Street)
-International Affairs
-International Affairs Extension
-International House
-Jerome Greene
-Jerome Greene Annex (Law School)
-Jerome Greene Hall (Law School)
-John Jay
-Journalism
-Kappa Alpha Theta (534 West 114th Street)
-Kappa Delta Rho (548 West 114th Street)
-Kent
-Knox Hall
-Kraft Center
-Lerner Hall
-Lewisohn
-Low Library
-Mathematics
-McBain
-McVickar Hall
-Mudd
-Northwest Corner Building
-Philosophy
-Prentis Hall
-President's House
-Psi Upsilon (542 West 114th Street)
-Pupin
-River
-Ruggles
-Schapiro
-Schapiro CEPSR
-Schermerhorn
-Schermerhorn Extension
-School of Social Work
-Service Building
-Shapiro Theater
-Sigma Chi (523 West 113th Street)
-Sigma Delta Tau (540 West 114th Street)
-Sigma Nu (556 West 113th Street)
-Sigma Phi Epsilon (550 West 113th Street)
-St. Paul's Chapel
-The School at Columbia
-University Hall
-Uris
-Wallach
-Watson
-Watt
-Wien
-William & June Warren (Business Law)
-William C. Warren Hall
-Woodbridge
-Zeta Psi (531 West 113th Street)
-
-Specs
-"""
